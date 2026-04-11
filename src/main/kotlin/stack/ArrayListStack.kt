c900c7b51a8ee3a3bbd02e1cb6edc4df1b8156cf
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2026-04-11 21:27:59 +0300

    Add ArrayListStack class

--- src/main/kotlin/stack/ArrayListStack.kt
+++ src/main/kotlin/stack/ArrayListStack.kt
@@ -1,5 +1,34 @@
 package org.example.stack
 
-class ArrayListStack : Stack {
-    // TODO: use delegation instead of inheritance here, use CustomArrayList
+import org.example.list.CustomArrayList
+import org.example.list.CustomList
+
+class ArrayListStack(l: CustomArrayList = CustomArrayList(0)) : Stack, CustomList by l {
+
+    override fun push(value: Int) {
+        add(value)
+    }
+
+    override fun pop(): Int {
+        try {
+            val popped = get(size - 1)
+            remove(get(size - 1))
+            return popped
+        } catch (_: ArrayIndexOutOfBoundsException) {
+            throw NoSuchElementException()
+        }
+    }
+
+    override fun peek(): Int {
+        try {
+            return get(size - 1)
+        } catch (_: ArrayIndexOutOfBoundsException) {
+            throw NoSuchElementException()
+        }
+    }
+
+    override val isEmpty: Boolean
+        get() {
+            return size == 0
+        }
 }
